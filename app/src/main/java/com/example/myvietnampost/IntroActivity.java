94cd7ce915906f1fea64e898b8b4edc5c4954d1b
OnTLinh <dinhduong26102000@gmail.com>
2023-11-30 18:47:49 +0700

    mới

--- app/src/main/java/com/example/myvietnampost/IntroActivity.java
+++ app/src/main/java/com/example/myvietnampost/IntroActivity.java
@@ -2,7 +2,13 @@ package com.example.myvietnampost;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        Handler handler =   new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                nextActivity();
+            }
+
+        },2000);
+    }
+
+    private void nextActivity() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user == null){
+            Intent intent = new Intent(IntroActivity.this,LoginActivity.class);
+            startActivity(intent);
+        }
+        else {
+            Intent intent = new Intent(IntroActivity.this,MainActivity.class);
+            startActivity(intent);
+        }
+        finish();
+
     }
 }
